2c8e03e0489236478c3ec215c8b5813a3b6a63e1
Fran <frangalo34@gmail.com>
2023-08-20 11:24:00 +0200

    Scrape titles from every item on the main page with css selector as follows ( li.active + li a::attr(href) )

--- Scraper.py
+++ Scraper.py
@@ -1,28 +1,33 @@
+import unittest
+from unittest.mock import Mock, patch, mock_open, call
+from scrapy.http import HtmlResponse
 import scrapy
-import json
+import json, logging
 from scrapy.crawler import CrawlerProcess
 
+
 class AnimeSpider(scrapy.Spider):
-          name = 'anime_spider'
-          start_urls = ['https://www3.animeflv.net/browse']
-          allowed_domains = ['animeflv.net']
+    name = 'anime_spider'
+    start_urls = ['https://www3.animeflv.net/browse']
+    allowed_domains = ['animeflv.net']
+
     custom_settings = {
             'LOG_LEVEL': logging.INFO 
     }
 
-          def parse(self, response):
-          
-                    titles = response.css('h3.Title::text').extract()
-                    titles = [title.strip() for title in titles if title.strip()]
+    def parse(self, response):
 
-                    # Yield the titles
-                    for title in titles:
-                              yield {'title': title}
+        titles = response.css('h3.Title::text').extract()
+        titles = [title.strip() for title in titles if title.strip()]
 
-          
-                    next_page = response.css('li.active + li a::attr(href)').get()
-                    if next_page:
-                              yield scrapy.Request(url=response.urljoin(next_page), callback=self.parse)
+        # Yield the titles
+        for title in titles:
+            yield {'title': title}
+
+
+        next_page = response.css('li.active + li a::attr(href)').get()
+        if next_page:
+            yield scrapy.Request(url=response.urljoin(next_page), callback=self.parse)
 
 
 class SaveToJsonPipeline:
